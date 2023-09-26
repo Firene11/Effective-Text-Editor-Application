3e0b28e3edd18a890fe1cd07f670878b62c3e2b3
Fih <124883284+Firene11@users.noreply.github.com>
2023-09-25 22:38:44 -0300

    added service worker to register route function

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,6 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -26,8 +25,8 @@ module.exports = () => {
       description: 'An effective text editor!',
       background_color: '#ff1493',
       theme_color: '#ff1493',
-      start_url: './',
-      publicPath: './',
+      start_url: '/',
+      publicPath: '/',
       icons: [
         {
           src: path.resolve('src/images/logo.png'),
@@ -42,7 +41,7 @@ module.exports = () => {
     }),
       
     ],
-
+// Add CSS loaders and babel to webpack.
     module: {
       rules: [
         {
