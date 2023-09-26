f24bec5b5b6531ea2d992b9ce4cfbc66c495529e
Fih <124883284+Firene11@users.noreply.github.com>
2023-09-25 21:14:48 -0300

    fixed curly brackets around my put function

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,8 @@ const initdb = async () =>
   });
 
 // Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.log('PUT to the database');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
 
   // Create a connection to the database database and version we want to use.
   const jateDb = await openDB('jate', 1);
@@ -30,12 +31,13 @@ export const putDb = async (content) => console.log('PUT to the database');
   // Get confirmation of the request.
   const result = await request;
   console.log('Data saved to the database', result);
+};
 
-
-
-
+//////////////////////////////
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.log('GET all from the database');
+
+
 
 initdb();
