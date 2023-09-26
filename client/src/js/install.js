0e138be3dff07bc0bec06a72cc34652a24c6cdef
Fih <124883284+Firene11@users.noreply.github.com>
2023-09-26 17:59:16 -0300

    trying to fix the install.js functions for event handlers

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,20 +3,24 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // Event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('beforeinstallprompt', event);
     event.preventDefault();
+    window.deferredPrompt = event;
     butInstall.style.visibility = 'visible';
-    textHeader.textContent = 'Click the button to install!';
+    //textHeader.textContent = 'Click the button to install!';
 
 // Click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    event.prompt();
+    const prompt = window.deferredPrompt;
     butInstall.setAttribute('disabled', true);
-    butInstall.textContent = 'Installed!';
+    event.prompt();
+    //butInstall.textContent = 'Installed!';
 });
 });
 
 // Handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    textHeader.textContent = 'App sucessfully Installed!';
+    //textHeader.textContent = 'App sucessfully Installed!';
     console.log('awesome!', 'appinstalled', event);
+    window.deferredPrompt = null;
 });
