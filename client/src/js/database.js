e1bdfe4879e8d99aca522f9884300b0a52dcc799
Fih <124883284+Firene11@users.noreply.github.com>
2023-09-25 21:04:39 -0300

    added put methods to the put function of the databse

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,9 +12,25 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.log('PUT to the database');
 
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .add() method on the store and pass in the content.
+  const request = store.add({ id: 1, value: content });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
 
 
 
